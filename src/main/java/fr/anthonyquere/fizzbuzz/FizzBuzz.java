6f67d7f44428552bb191fa6e3d80a5f2b40fa2ff
Lyune <laurency.yuna@gmail.com>
2025-03-11 11:53:46 +0100

    Updating startFizzBuzz. All tests passed.

--- src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,6 @@
 package fr.anthonyquere.fizzbuzz;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FizzBuzz {
@@ -9,6 +10,24 @@ public class FizzBuzz {
     }
 
     public static List<String> startFizzBuzz(int count) {
-        return null;
+        List<String> fizzbuzz = new ArrayList<>();
+        for (int i=0; i<count; ++i){
+            var index = i+1;
+            if(index % 3 == 0 && index % 5 == 0){
+                fizzbuzz.add("FizzBuzz");
+            }
+            else if(index % 3 == 0){
+                fizzbuzz.add("Fizz");
+            }
+            else if(index % 5 == 0) {
+                fizzbuzz.add("Buzz");
+            }
+            else{
+                fizzbuzz.add(Integer.toString(index));
+            }
+        }
+        return fizzbuzz;
     }
+
+
 }
