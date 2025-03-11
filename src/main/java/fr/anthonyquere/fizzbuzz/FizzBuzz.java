e7e4d05f58668bfe0e4620983e4bb014cae6500c
Lyune <laurency.yuna@gmail.com>
2025-03-11 10:35:40 +0100

    Commit test on FizzBuzz

--- src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class FizzBuzz {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! What's up?");
     }
 
     public static List<String> startFizzBuzz(int count) {
