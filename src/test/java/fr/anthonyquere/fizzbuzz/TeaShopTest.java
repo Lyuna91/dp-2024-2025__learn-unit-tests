e27edba28f8984bfb66b746de6496be939ee4c29
Lyune <laurency.yuna@gmail.com>
2025-03-22 14:29:04 +0100

    Adding a test and updating setUp

--- src/test/java/fr/anthonyquere/fizzbuzz/TeaShopTest.java
+++ src/test/java/fr/anthonyquere/fizzbuzz/TeaShopTest.java
@@ -10,15 +10,17 @@ import org.junit.jupiter.api.Test;
 
 public class TeaShopTest {
 
-    private Tea tea;
+    private Tea tea1;
     private TeaCup teaCup;
     private TeaShop teaShop;
 
     @BeforeEach
     void setUp(){
-        tea = new Tea("Black Tea", 80, 80, true);
-        teaCup = new TeaCup();
         teaShop = new TeaShop(100);
+        teaCup = new TeaCup();
+        tea1 = new Tea("Black Tea", 80, 80, true);
+        Tea tea2 = new Tea("Oolong Tea", 60, 85, false);
+        teaShop.addTea(tea2);
     }
     @Test
     void should_not_be_drinkable_if_empty(){
@@ -38,24 +40,32 @@ public class TeaShopTest {
     void should_not_be_drinkable_if_temperature_is_not_ideal(){
         teaCup.addWater(90);
 
-        // Une tasse avec une température pas idéale n'est pas buvable
+        // Une tasse avec une température pas idéale n'est pas buvable.
         assertThat(teaCup.isReadyToDrink()).isFalse();
     }
 
     @Test
     void should_not_be_drinkable_without_steeping_time(){
         teaCup.addWater(80);
-        teaCup.addTea(tea);
+        teaCup.addTea(tea1);
 
-        // Tasse qui n'a pas eu le temps d'infuser n'est pas buvable
+        // Tasse qui n'a pas eu le temps d'infuser n'est pas buvable.
         assertThat(teaCup.isReadyToDrink()).isFalse();
     }
 
     @Test
-    void should_prepare_tea(){
-        teaShop.addTea(tea);
+    void should_add_tea(){
+        teaShop.addTea(tea1);
         teaCup = teaShop.prepareTea("Black Tea");
 
+        // Tester l'ajout d'un thé indirectement en le préparant.
+        assertThat(teaCup).isNotNull();
+    }
+    @Test
+    void should_prepare_tea(){
+        teaCup = teaShop.prepareTea("Oolong Tea");
+
+        // Tester la préparation d'un thé déjà dans le Shop.
         assertThat(teaCup).isNotNull();
     }
 
@@ -63,6 +73,7 @@ public class TeaShopTest {
     void should_be_valid_water_temperature(){
         teaShop.setWaterTemperature(59);
 
+        // Tester la nouvelle température indirectement
         assertThat(teaShop).isNotNull();
     }
 }
